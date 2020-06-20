fcc884943bbbe40b4d48db4c702c857353d62c35
se <ss@MBP-Ozzy.Dlink>
2020-06-20 13:37:53 +0300

    init

---
+++ api/routes/products.js
@@ -0,0 +1,16 @@
+// const { Router } = require('express')
+// const router = Router();
+
+// router.get('/', (req, res, next) => {
+//   res.status(200).json({
+//     message: 'handling get request to /products'
+//   })
+// })
+
+// router.post('/', (req, res, next) => {
+//   res.status(200).json({
+//     message: 'handling post request to /products'
+//   })
+// })
+
+// router.export = router;
---
+++ app.js
@@ -0,0 +1,9 @@
+//const express = require('express')
+//const app = express();
+
+//const productRoutes = require('./api/routes/products')
+//app.use('/products', productRoutes)
+//app.use(app.router);
+//routes.initialize(app);
+
+//module.exports = app;
